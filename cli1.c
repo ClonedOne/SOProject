ebdd51f4526ca70a26a9b0ae89804a8119c49d5c
ClonedOne <joedeklo@hotmail.it>
2014-03-11 20:21:28 +0100

    almost like an artwork

--- cli1.c
+++ cli1.c
@@ -15,14 +15,79 @@ The goal of all of it is just to create a sinchronous communication channel betw
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
+#include <fcntl.h>
 
 #define DIM 1024
+#define ACCEPT 1
+#define DISCONNECT -2
+#define QUIT -1
+
 struct winsize ts;
 char* callingIP;
 char callingID[DIM];
 int row_count;
 int inchat;
+int com_res;
 
+
+
+//This combination of escape characters does the following: 
+//Erase screen, Moves the cursor back to home position
+//Moves the cursor up a line to compensate for puts' line feed
+//prints the formatted info about the caller
+void stampaHeader(){
+	puts("\033[2J\033[H");
+	printf("\033[AYou have been called by: \033[1;31m%s\033[0m\n",callingIP);
+	printf("Caller's name is: \033[1;31m%s\033[0m\n", callingID);
+	printf("Type: '\033[1;32m::h\033[0m' for the list of commands\n\n");
+	row_count = 0;
+}
+
+
+//This function manages the commands input by the user
+int command(char* buf) {
+	int k;
+	char letter = buf[2];
+	switch(letter){
+		case 'H':
+		case 'h':
+			printf("\033[A\033[2K");
+			puts("Welcome to the HELP, here is the list of possbile commands.");
+			puts("::a --> Accept incoming call");
+			puts("::c --> Call someone"); //TO BE DONE
+			puts("::d --> Disconnect from current call");
+			puts("::q --> Quit the program");
+			puts("::h --> Show the HELP");	
+		break;
+		
+		
+		case 'A':
+		case 'a':
+			return ACCEPT;
+		break;
+		
+		
+		case 'D':
+		case 'd':
+			puts("Disconnecting...");
+			sleep(1);
+			printf("\033[2J");
+			for(k = 0; k < ts.ws_row; k++)
+				printf("%s", "\033[A");
+			return DISCONNECT;
+		break;
+	
+		case 'Q':
+		case 'q':
+			return QUIT;
+		break;
+	
+		default:
+			puts("Couldn't recognise the command, please try '::h' for help");
+		
+	}
+	return 0;
+}
 
 
 //This function is used to keep track of changes in the size of the terminal window
@@ -30,14 +95,8 @@ void sigwinch_handler (int a) {
 	signal(SIGWINCH, sigwinch_handler);
 	ioctl(0, TIOCGWINSZ, &ts);
 	if(inchat == 1){
-	//PROBLEM: when the resizing is downward, while it still does the job, the screen does not scroll down to compensate
-	//This combination of escape characters does the following: 
-	//Erase screen, Moves the cursor back to home position, Moves the cursor up a line to compensate for puts' line feed
-		puts("\033[2J\033[H");
-		printf("\033[AYou have been called by: %s\n",callingIP);
-		printf("Caller name is: %s\n", callingID);
-		printf("Type: '-c h' for the list of commands\n\n");
-		row_count = 0;
+	//PROBLEM: when the resizing is downward the screen does not scroll down to compensate
+		stampaHeader();
 	}
 }
 
@@ -50,18 +109,14 @@ void* func_t_1 () {
 	int sock_a;
 	int control;
 	int length;
+	int rN;
+	int wN;
+	int k;
 	struct sockaddr_in server;
 	struct sockaddr_in client;
 	short port = 4000;
 	char sendBuf[DIM];
 	char recvBuf[DIM];
-	char* comm;
-	
-	
-	//this struct is used to store the dimensions (rows/col) of the terminal window
-	ioctl(0, TIOCGWINSZ, &ts);
-	signal(SIGWINCH, sigwinch_handler);
-	row_count = 0;
 	
 	
 	puts("We are starting the receiving thread");
@@ -94,47 +149,77 @@ void* func_t_1 () {
 	}
 	puts("The process is now listening");
 
-
-	//waits for connections
-	length = sizeof(client);
-	puts("We are waiting to accept a connection from the other users");
-	while ((sock_a = accept(servsock, (struct sockaddr *)&client, &length)) == -1);
-	puts("Connection Accepted");
-
-
-	callingIP = inet_ntoa(client.sin_addr);
-	printf("You have been called by: %s\n",callingIP);
-
+	while(com_res != QUIT){
 	
-	//I'll probably will let the server/other client do this part of exchanging names
-	comm = "Hello, please state your name";
-	strcpy(sendBuf, comm);
-	//printf("DEBUG: %s\n", comm);
-	write(sock_a, sendBuf, DIM);
-	read(sock_a, recvBuf, DIM);
-	strcpy(callingID, recvBuf);
-	printf("caller's name is: %s\n", callingID);
-	printf("Type: '-c h' for the list of commands\n\n");
+		//waits for connections
+		length = sizeof(client);
+		puts("\033[1;34mWaiting for incoming calls.....\033[0m");
+		while ((sock_a = accept(servsock, (struct sockaddr *)&client, &length)) == -1 );
+		
+		//clears the screen
+		printf("\033[2J");
+		for(k = 0; k < ts.ws_row; k++)
+			printf("%s", "\033[A");
 	
-	while (1){
-		inchat = 1;
+		//Gets caller's IP and name and ask the user to accept the call
+		callingIP = inet_ntoa(client.sin_addr);
+		read(sock_a, recvBuf, DIM);
+		strcpy(callingID, recvBuf);
+		recvBuf[0] = '\0';
+		stampaHeader();
+		printf("Do you want to accept the incoming call?\n");
+		fgets(sendBuf, DIM, stdin);
+		com_res = command(sendBuf);
+		sendBuf[0] = '\0';
+	
+		if(com_res == ACCEPT){
+	
+			//change the communicating socket and STDIN to NONBLOCKING mode
+			int flags = fcntl(sock_a, F_GETFL, 0);
+			fcntl(sock_a, F_SETFL, flags | O_NONBLOCK);
+			int fd = fileno(stdin);
+			flags = fcntl(fd, F_GETFL, 0);
+			fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	
+			inchat = 1;
+			raise(SIGWINCH);
+	
+			while(!(com_res <= 0)){
+				rN = read(sock_a, recvBuf, DIM);
+				if(rN > 0){
+					printf("%s", "\033[2K");
+					for(k = 0; k < ts.ws_col; k++)
+						printf("%s", "\033[D");
+					printf("\033[1;31m%s: \033[0m", callingID);
+					printf("%s", recvBuf);
+					row_count++;
+					recvBuf[0] = '\0';
+				}
+				if((fgets(sendBuf, DIM, stdin)) != NULL) {
+					if(sendBuf[0] == ':' && sendBuf[1] == ':') {
+						com_res = command(sendBuf);
+						sendBuf[0] = '\0';
+					}else{
+						printf("\033[A\033[2K");
+						printf("\033[1;34m%s: \033[0m", "You");
+						printf("%s", sendBuf);
+						write(sock_a, sendBuf, DIM);
+						sendBuf[0] = '\0';
+						row_count++;
+					}
+				}
 		
-		//I need to find a way to make "read" non blocking and possibly interrupt driven
-		printf( "%-*s\n", ts.ws_col, "ABC" );
-		
-		
-		row_count++;	
-		if (row_count == ts.ws_row -4){
-			puts("\033[2J\033[H");
-			printf("\033[AYou have been called by: %s\n",callingIP);
-			printf("Caller name is: %s\n", callingID);
-			printf("Type: '-c h' for the list of commands\n\n");
-			 row_count = 0;
+				if(row_count == ts.ws_row -4){
+					stampaHeader();
+				}
+	
+			}
 		}
-	
-	
+		else{
+			close(sock_a);
+			puts("Call refused");
+		}
 	}
-
 }
 
 void* func_t_2 (){}
@@ -143,10 +228,23 @@ int main(int argc, char*argv[]){
 
 	pthread_t t1;
 	int retT1;
+	int k;
 
+	//this struct is used to store the dimensions (rows/col) of the terminal window
+	ioctl(0, TIOCGWINSZ, &ts);
+	signal(SIGWINCH, sigwinch_handler);
+	row_count = 0;
+
+	printf("\033[2J");
+	for(k = 0; k < ts.ws_row; k++)
+	printf("%s", "\033[A");
+	
+	
 	puts("Welcome! We are now starting the service!");
 	retT1 = pthread_create(&t1, NULL, &func_t_1, NULL);
 	if (retT1 != 0)
 		puts("Couldn't create the thread!");
-	while(1);//STOOOOOOOOPIDO!
+		
+	//This loop ends when the user input the ::q command
+	while(com_res != QUIT);
 }
--- testcli1.c
+++ testcli1.c
@@ -5,6 +5,8 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+
 
 #define MAX_DIM 1024
 #define MAX_CODA 3
@@ -17,7 +19,7 @@ void main(){
 	struct sockaddr_in client;
 	struct hostent *hp;
 	char buff[MAX_DIM];
-
+	char* name = "ClonedTest1";
 //crea la socket per inviare dati
 	ds_sock = socket (AF_INET, SOCK_STREAM, 0);
 	
@@ -28,18 +30,34 @@ void main(){
 		puts("FAAK LOCALHOST");
 	}
 	bcopy(hp->h_addr, &client.sin_addr, hp->h_length);
-
-//	memcpy(&client.sin_addr, hp->h_addr_list[0], hp->h_length);
 	
 	res = connect(ds_sock,(struct sockaddr *)&client,sizeof(client));
 	if (res == -1){
 		puts("ERRORE CONNECT!!");
 	}
 	
-	printf("stringhe\n");
-	read(ds_sock, buff, MAX_DIM);
-	printf("%s\n", buff);
-	scanf("%s", buff);
-	write(ds_sock, buff, MAX_DIM);
+	write(ds_sock, name, strlen(name));
+	
+	
+	//change the communicating socket to NONBLOCKING mode
+	int flags = fcntl(ds_sock, F_GETFL, 0);
+	fcntl(ds_sock, F_SETFL, flags | O_NONBLOCK);
+	int rN;
+	int fd = fileno(stdin);
+	flags = fcntl(fd, F_GETFL, 0);
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	
+	while(1){
+		rN = read(ds_sock, buff, MAX_DIM);
+		if (rN > 0){
+			printf("%s\n", buff);
+			buff[0] = '\0';
+		}
+	
+		if((fgets(buff, MAX_DIM, stdin)) != NULL) {
+			write(ds_sock, buff, MAX_DIM);
+			buff[0] = '\0';
+		}
+	}
 }
 
