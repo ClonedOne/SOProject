f84cde51de8fd61dc11f643a2af6ff0705b98f41
uncleman11 <darth.sidius92@gmail.com>
2014-08-25 17:13:13 +0200

    Parser per password e login pronto. Iniziata stesura Server, accetta connessioni concorrenti e login

--- client_parser.c
+++ client_parser.c
@@ -10,57 +10,68 @@
 #define USERNAME_HEADER_LENGTH 5 
 #define STATUS_HEADER_LENGTH 7
 #define IP_HEADER_LENGTH 3
+#define SEPARATOR_LENGTH 2
 #define FILENAME "usersInfo.txt"
 
 
 int getInfoByUser(char* user, char** info)
 {
-	int fd;
+	int fd,found;
 	FILE *fp;
 	
 	char* buf = malloc(MAX_USERNAME_LENGTH+USERNAME_HEADER_LENGTH);
 	char* readUser = malloc(MAX_USERNAME_LENGTH);
 	char* status = malloc(MAX_STATUS_LENGTH);
 	char* ip = malloc(MAX_IP_LENGTH);
+	char* separator = malloc(SEPARATOR_LENGTH);
 
 	
 	fp=fopen(FILENAME,"r");
-	fgets(buf,MAX_USERNAME_LENGTH+USERNAME_HEADER_LENGTH+2,fp);
-	buf[strlen(buf)-1]='\0';
-	buf+=USERNAME_HEADER_LENGTH;
+
+	found=0;
 	
-	
-	strcpy(readUser,buf);
-	
-	
-	if(strcmp(readUser,user)==0)
+	while(found!=1)
 	{
-		buf=malloc(MAX_STATUS_LENGTH+STATUS_HEADER_LENGTH+2);
-		fgets(buf,MAX_STATUS_LENGTH+STATUS_HEADER_LENGTH+2,fp);
+
+		fgets(buf,MAX_USERNAME_LENGTH+USERNAME_HEADER_LENGTH+2,fp);
 		buf[strlen(buf)-1]='\0';
+		buf+=USERNAME_HEADER_LENGTH;
+	
+	
+		strcpy(readUser,buf);
+	
+	
+		if(strcmp(readUser,user)==0)
+		{
+			buf=malloc(MAX_STATUS_LENGTH+STATUS_HEADER_LENGTH+2);
+			fgets(buf,MAX_STATUS_LENGTH+STATUS_HEADER_LENGTH+2,fp);
+			buf[strlen(buf)-1]='\0';
 		
-		buf+=STATUS_HEADER_LENGTH;
-		strcpy(status,buf);
-		info[0]=status;
+			buf+=STATUS_HEADER_LENGTH;
+			strcpy(status,buf);
+			info[0]=status;
 		
-		buf=malloc(MAX_IP_LENGTH+IP_HEADER_LENGTH+2);
-		fgets(buf,MAX_IP_LENGTH+IP_HEADER_LENGTH+2,fp);
-		buf[strlen(buf)-1]='\0';
-		buf+=IP_HEADER_LENGTH;
-		strcpy(ip,buf);
-		info[1]=ip;
+			buf=malloc(MAX_IP_LENGTH+IP_HEADER_LENGTH+2);
+			fgets(buf,MAX_IP_LENGTH+IP_HEADER_LENGTH+2,fp);
+			buf[strlen(buf)-1]='\0';
+			buf+=IP_HEADER_LENGTH;
+			strcpy(ip,buf);
+			info[1]=ip;
+
+			found=1;
 		
-		return 1;
 		
+		}
+		else
+		{
+			//DA COMPLETARE SCORRERE ALTRE RIGHE DEL FILE
+
+			buf=malloc(SEPARATOR_LENGTH);
+			if(fgets(buf,SEPARATOR_LENGTH,fp)==NULL)
+				return 1;
+
 		
-	}
-	else
-	{
-		//DA COMPLETARE SCORRERE ALTRE RIGHE DEL FILE
-		
-		info[0]="cacca";
-		info[1]="zero";
-		return 0;
+		}
 	}
 	
 	
--- serv1.c
+++ serv1.c
@@ -10,10 +10,52 @@
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include "login_parser.h"
 
 
 #define CODA 5
 #define DIM 1024
+#define MAX_PENDING_CONNECTIONS 100
+
+int clientConnection(int* cli_sock)
+{
+
+	int r_size;
+	int control;
+	char* user;
+	char* password;
+	
+	user = malloc(MAX_USERNAME_LENGTH);
+	password=malloc(MAX_PASSWORD_LENGTH);
+
+	
+	//Client should login
+
+	do {
+		if ((r_size = read(cli_sock, user, MAX_USERNAME_LENGTH)) == -1){
+			perror("Read error");
+		}
+
+		if ((r_size = read(cli_sock, password, MAX_PASSWORD_LENGTH)) == -1){
+			perror("Read error");
+		}
+
+
+		/*if ((control = write(cli_sock, buff, r_size)) == -1){
+			perror("Write error");
+		}*/
+
+	}while (r_size > 0);
+
+	if(checkUserLogin(user,password)==1)
+		puts("User Logged");
+	else
+		puts("Bad Login");
+	pthread_exit(0);
+	
+}
+
 
 void main() {
 
@@ -25,8 +67,9 @@ void main() {
 	int control;
 	int src_file;
 	int r_size;
-	char buff[DIM];
+	char* buff;
 	char f_name[DIM];
+	pthread_t* tid;
 	struct sockaddr_in server;
 	struct sockaddr client;
 	
@@ -59,6 +102,7 @@ void main() {
 	}
 	puts("The process is now listening");
 
+/*
 //asks the user for the list file name and opens it
 	while(control != 1){
 		puts("Please insert the name of the list file");
@@ -71,19 +115,49 @@ void main() {
 			control = 1;
 	}
 	
-	length = sizeof(client);
+	length = sizeof(client);*/
 	
 //accept incoming connections
-	while ((cli_sock = accept(serv_sock, &client, & length)) == -1);
-	
-	do {
-		if ((r_size = read(src_file, buff, DIM)) == -1){
-			perror("Read error");
-		}
-		if ((control = write(cli_sock, buff, r_size)) == -1){
-			perror("Write error");
-		}
 
-	}while (r_size > 0);
+tid=(pthread_t*)malloc(MAX_PENDING_CONNECTIONS*sizeof(pthread_t));
+int i=0;
+
+while(1)
+{
+	
+	while ((cli_sock = accept(serv_sock, &client, & length)) == -1);
+
+	pthread_create(&(tid[i]),NULL,(void*)&clientConnection,(void *) &cli_sock);
+	i++;
+}	
+
+
+
+//Wait user's login
+	
+	
+	
+	
+	
+	
+}
+
+
+
+int checkUserLogin(char* user, char* password)
+{
+	int found;
+	char** info = malloc(sizeof(char*)*2);
+	found = getInfoByUser(user,info);
+
+	if(found!=0)	
+	{	
+		if(strcmp(password,info[0])==0)
+			return 1;
+		else
+			return 0;		
+	}
+
+	return 0;
 
 }
